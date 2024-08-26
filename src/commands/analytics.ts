3e87983d9774c00d829d9722c7e258c83158935d
Charlie Gerard <charlie.a.gerard@gmail.com>
2024-08-26 12:35:43 -0700

    wip

--- src/commands/analytics.ts
+++ src/commands/analytics.ts
@@ -4,7 +4,7 @@ import contrib from 'blessed-contrib'
 import meow from 'meow'
 import ora from 'ora'
 
-import { outputFlags, validationFlags } from '../flags'
+import { outputFlags } from '../flags'
 import { handleApiCall, handleUnsuccessfulApiResponse } from '../utils/api-helpers'
 import { AuthError, InputError } from '../utils/errors'
 import { printFlagList } from '../utils/formatting'
@@ -12,9 +12,11 @@ import { getDefaultKey, setupSdk } from '../utils/sdk'
 
 import type { CliSubcommand } from '../utils/meow-with-subcommands'
 import type { Ora } from "ora"
+import chalk from 'chalk'
 
 export const analytics: CliSubcommand = {
-  description: 'Look up analytics data',
+  description: `Look up analytics data \n
+  Default parameters are set to show the organization-level analytics over the last 7 days.`,
   async run (argv, importMeta, { parentName }) {
     const name = parentName + ' analytics'
 
@@ -36,31 +38,53 @@ export const analytics: CliSubcommand = {
   }
 }
 
+const analyticsFlags: { [key: string]: any } = {
+  scope: {
+    type: 'string',
+    shortFlag: 's',
+    default: 'org',
+    description: "Scope of the analytics data - either 'org' or 'repo'"
+  },
+  time: {
+    type: 'number',
+    shortFlag: 't',
+    default: 7,
+    description: 'Time filter - either 7, 30 or 90'
+  },
+  repo: {
+    type: 'string',
+    shortFlag: 'r',
+    default: '',
+    description: "Name of the repository"
+  },
+}
+
 // Internal functions
 
 type CommandContext = {
   scope: string
-  time: string
-  repo: string | undefined
+  time: number
+  repo: string
   outputJson: boolean
 }
 
 function setupCommand (name: string, description: string, argv: readonly string[], importMeta: ImportMeta): void|CommandContext {
   const flags: { [key: string]: any } = {
     ...outputFlags,
-    ...validationFlags,
+    ...analyticsFlags
   }
 
   const cli = meow(`
     Usage
-      $ ${name} <scope> <time>
+      $ ${name} --scope=<scope> --time=<time filter>
 
     Options
       ${printFlagList(flags, 6)}
 
     Examples
-      $ ${name} org 7
-      $ ${name} org 30
+      $ ${name} --scope=org --time=7
+      $ ${name} --scope=org --time=30
+      $ ${name} --scope=repo --repo=test-repo --time=30
   `, {
     argv,
     description,
@@ -69,43 +93,36 @@ function setupCommand (name: string, description: string, argv: readonly string[
   })
 
   const {
-    json: outputJson
+    json: outputJson,
+    scope, 
+    time,
+    repo
   } = cli.flags
 
-  const scope = cli.input[0]
-
-  if (!scope) {
-    throw new InputError('Please provide a scope to get analytics data')
+  if (scope !== 'org' && scope !== 'repo') {
+    throw new InputError("The scope must either be 'org' or 'repo'")
   }
 
-  if (!cli.input.length) {
-    throw new InputError('Please provide a scope and a time to get analytics data')
+  if (time !== 7 && time !== 30 && time !== 90) {
+    throw new InputError('The time filter must either be 7, 30 or 90')
   }
 
-  if (scope && !['org', 'repo'].includes(scope)) {
-    throw new InputError("The scope must either be 'scope' or 'repo'")
-  }
-
-  const repo = scope === 'repo' ? cli.input[1] : undefined
-
-  const time = scope === 'repo' ? cli.input[2] : cli.input[1]
-
-  if (!time) {
-    throw new InputError('Please provide a time to get analytics data')
-  }
-
-  if (time && !['7', '30', '60'].includes(time)) {
-    throw new InputError('The time filter must either be 7, 30 or 60')
+  if(scope === 'repo' && !repo){
+    console.error(
+      `${chalk.bgRed.white('Input error')}: Please provide a repository name when using the repository scope. \n`
+    )
+    cli.showHelp()
+    return
   }
 
   return <CommandContext>{
-      scope, time, repo, outputJson
+    scope, time, repo, outputJson
   }
 }
 
-async function fetchOrgAnalyticsData (time: string, spinner: Ora, apiKey: string, outputJson: boolean): Promise<void> {
+async function fetchOrgAnalyticsData (time: number, spinner: Ora, apiKey: string, outputJson: boolean): Promise<void> {
   const socketSdk = await setupSdk(apiKey)
-  const result = await handleApiCall(socketSdk.getOrgAnalytics(time), 'fetching analytics data')
+  const result = await handleApiCall(socketSdk.getOrgAnalytics(time.toString()), 'fetching analytics data')
 
   if (result.success === false) {
     return handleUnsuccessfulApiResponse('getOrgAnalytics', result, spinner)
@@ -113,132 +130,45 @@ async function fetchOrgAnalyticsData (time: string, spinner: Ora, apiKey: string
 
   spinner.stop()
 
-  const data = result.data.reduce((acc: { [key: string]: any }, current) => {
-    const formattedDate = new Date(current.created_at).toLocaleDateString()
+  if(!result.data.length){
+    return console.log('No analytics data is available for this organization yet.')
+  }
 
-    if (acc[formattedDate]) {
-      acc[formattedDate].total_critical_alerts += current.total_critical_alerts
-      acc[formattedDate].total_high_alerts += current.total_high_alerts
-      acc[formattedDate].total_critical_added += current.total_critical_added
-      acc[formattedDate].total_high_added += current.total_high_added
-      acc[formattedDate].total_critical_prevented += current.total_critical_prevented
-      acc[formattedDate].total_high_prevented += current.total_high_prevented
-      acc[formattedDate].total_medium_prevented += current.total_medium_prevented
-      acc[formattedDate].total_low_prevented += current.total_low_prevented
-    } else {
-      acc[formattedDate] = current
-      acc[formattedDate].created_at = formattedDate
-    }
-
-    return acc
-  }, {})
+  const data = formatData(result.data)
 
   if(outputJson){
     return console.log(data)
   }
 
-  const screen = blessed.screen()
-  // eslint-disable-next-line
-  const grid = new contrib.grid({rows: 4, cols: 4, screen})
-
-  renderLineCharts(grid, screen, 'Total critical alerts', [0,0,1,2], data, 'total_critical_alerts')
-  renderLineCharts(grid, screen, 'Total high alerts', [0,2,1,2], data, 'total_high_alerts')
-  renderLineCharts(grid, screen, 'Total critical alerts added to main', [1,0,1,2], data, 'total_critical_added')
-  renderLineCharts(grid, screen, 'Total high alerts added to main', [1,2,1,2], data, 'total_high_added')
-  renderLineCharts(grid, screen, 'Total critical alerts prevented from main', [2,0,1,2], data, 'total_critical_prevented')
-  renderLineCharts(grid, screen, 'Total high alerts prevented from main', [2,2,1,2], data, 'total_high_prevented')
-
-  const bar = grid.set(3, 0, 1, 2, contrib.bar,
-      { label: 'Top 5 alert types'
-      , barWidth: 10
-      , barSpacing: 17
-      , xOffset: 0
-      , maxHeight: 9, barBgColor: 'magenta' })
-
-   screen.append(bar) //must append before setting data
-
-   const top5AlertTypes = Object.values(data)[0].top_five_alert_types
-   
-   bar.setData(
-      { titles: Object.keys(top5AlertTypes)
-      , data: Object.values(top5AlertTypes)})
-
-  screen.render()
-    
-  screen.key(['escape', 'q', 'C-c'], function() {
-    return process.exit(0);
-  })
+  return displayAnalyticsScreen(data)
 }
 
-async function fetchRepoAnalyticsData (repo: string, time: string, spinner: Ora, apiKey: string, outputJson: boolean): Promise<void> {
+async function fetchRepoAnalyticsData (repo: string, time: number, spinner: Ora, apiKey: string, outputJson: boolean): Promise<void> {
   const socketSdk = await setupSdk(apiKey)
-  const result = await handleApiCall(socketSdk.getRepoAnalytics(repo, time), 'fetching analytics data')
+  const result = await handleApiCall(socketSdk.getRepoAnalytics(repo, time.toString()), 'fetching analytics data')
 
   if (result.success === false) {
     return handleUnsuccessfulApiResponse('getRepoAnalytics', result, spinner)
   }
   spinner.stop()
 
-  const data = result.data.reduce((acc: { [key: string]: any }, current) => {
-    const formattedDate = new Date(current.created_at).toLocaleDateString()
+  if(!result.data.length){
+    return console.log('No analytics data is available for this organization yet.')
+  }
 
-    if (acc[formattedDate]) {
-      acc[formattedDate].total_critical_alerts += current.total_critical_alerts
-      acc[formattedDate].total_high_alerts += current.total_high_alerts
-      acc[formattedDate].total_critical_added += current.total_critical_added
-      acc[formattedDate].total_high_added += current.total_high_added
-      acc[formattedDate].total_critical_prevented += current.total_critical_prevented
-      acc[formattedDate].total_high_prevented += current.total_high_prevented
-      acc[formattedDate].total_medium_prevented += current.total_medium_prevented
-      acc[formattedDate].total_low_prevented += current.total_low_prevented
-    } else {
-      acc[formattedDate] = current
-      acc[formattedDate].created_at = formattedDate
-    }
-
-    return acc
-  }, {})
+  const data = formatData(result.data)
 
   if(outputJson){
     return console.log(data)
   }
 
-  const screen = blessed.screen()
-  // eslint-disable-next-line
-  const grid = new contrib.grid({rows: 4, cols: 4, screen})
-
-  renderLineCharts(grid, screen, 'Total critical alerts', [0,0,1,2], data, 'total_critical_alerts')
-  renderLineCharts(grid, screen, 'Total high alerts', [0,2,1,2], data, 'total_high_alerts')
-  renderLineCharts(grid, screen, 'Total critical alerts added to main', [1,0,1,2], data, 'total_critical_added')
-  renderLineCharts(grid, screen, 'Total high alerts added to main', [1,2,1,2], data, 'total_high_added')
-  renderLineCharts(grid, screen, 'Total critical alerts prevented from main', [2,0,1,2], data, 'total_critical_prevented')
-  renderLineCharts(grid, screen, 'Total high alerts prevented from main', [2,2,1,2], data, 'total_high_prevented')
-
-  const bar = grid.set(3, 0, 1, 2, contrib.bar,
-      { label: 'Top 5 alert types'
-      , barWidth: 10
-      , barSpacing: 17
-      , xOffset: 0
-      , maxHeight: 9, barBgColor: 'magenta' })
-
-   screen.append(bar) //must append before setting data
-
-   const top5AlertTypes = Object.values(data)[0].top_five_alert_types
-   
-   bar.setData(
-      { titles: Object.keys(top5AlertTypes)
-      , data: Object.values(top5AlertTypes)})
-
-  screen.render()
-    
-  screen.key(['escape', 'q', 'C-c'], function() {
-    return process.exit(0);
-  })
+  return displayAnalyticsScreen(data)
 }
 
-const renderLineCharts = (grid: any, screen: any, title: string, coords: number[], data: {[key: string]: {[key: string]: number}}, label: string) => {
+const renderLineCharts = (grid: any, screen: any, title: string, coords: number[], data: FormattedAnalyticsData, label: string) => {
   const formattedDates = Object.keys(data).map(d => `${new Date(d).getMonth()+1}/${new Date(d).getDate()}`)
 
+  // @ts-ignore
   const alertsCounts = Object.values(data).map(d => d[label])
   
   const line = grid.set(...coords, contrib.line,
@@ -266,4 +196,110 @@ const renderLineCharts = (grid: any, screen: any, title: string, coords: number[
   }
 
   line.setData([lineData])
+}
+
+type AnalyticsData = {
+  id: number,
+  created_at: string
+  repository_id: string
+  organization_id: number
+  repository_name: string
+  total_critical_alerts: number
+  total_high_alerts: number
+  total_medium_alerts: number
+  total_low_alerts: number
+  total_critical_added: number
+  total_high_added: number
+  total_medium_added: number
+  total_low_added: number
+  total_critical_prevented: number
+  total_high_prevented: number
+  total_medium_prevented: number
+  total_low_prevented: number
+  top_five_alert_types: {
+    [key: string]: number
+  }
+}
+
+type FormattedAnalyticsData = {
+  [key: string]: AnalyticsData
+}
+
+const formatData = (data: AnalyticsData[]) => {
+  return data.reduce((acc: { [key: string]: any }, current) => {
+    const formattedDate = new Date(current.created_at).toLocaleDateString()
+
+    if (acc[formattedDate]) {
+      acc[formattedDate].total_critical_alerts += current.total_critical_alerts
+      acc[formattedDate].total_high_alerts += current.total_high_alerts
+      acc[formattedDate].total_critical_added += current.total_critical_added
+      acc[formattedDate].total_high_added += current.total_high_added
+      acc[formattedDate].total_critical_prevented += current.total_critical_prevented
+      acc[formattedDate].total_high_prevented += current.total_high_prevented
+      acc[formattedDate].total_medium_prevented += current.total_medium_prevented
+      acc[formattedDate].total_low_prevented += current.total_low_prevented
+    } else {
+      acc[formattedDate] = current
+      acc[formattedDate].created_at = formattedDate
+    }
+
+    return acc
+  }, {})
+}
+
+const displayAnalyticsScreen = (data: FormattedAnalyticsData) => {
+  const screen = blessed.screen()
+  // eslint-disable-next-line
+  const grid = new contrib.grid({rows: 4, cols: 4, screen})
+
+  renderLineCharts(grid, screen, 'Total critical alerts', [0,0,1,2], data, 'total_critical_alerts')
+  renderLineCharts(grid, screen, 'Total high alerts', [0,2,1,2], data, 'total_high_alerts')
+  renderLineCharts(grid, screen, 'Total critical alerts added to main', [1,0,1,2], data, 'total_critical_added')
+  renderLineCharts(grid, screen, 'Total high alerts added to main', [1,2,1,2], data, 'total_high_added')
+  renderLineCharts(grid, screen, 'Total critical alerts prevented from main', [2,0,1,2], data, 'total_critical_prevented')
+  renderLineCharts(grid, screen, 'Total high alerts prevented from main', [2,2,1,2], data, 'total_high_prevented')
+
+  const bar = grid.set(3, 0, 1, 2, contrib.bar,
+      { label: 'Top 5 alert types'
+      , barWidth: 10
+      , barSpacing: 17
+      , xOffset: 0
+      , maxHeight: 9, barBgColor: 'magenta' })
+
+   screen.append(bar) //must append before setting data
+ 
+   const top5 = extractTop5Alerts(data)
+   
+   bar.setData(
+      { titles: Object.keys(top5)
+      , data: Object.values(top5)})
+
+  screen.render()
+    
+  screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
+}
+
+const extractTop5Alerts = (data: FormattedAnalyticsData) => {
+  const allTop5Alerts = Object.values(data).map(d => d.top_five_alert_types)
+  
+  const aggTop5Alerts = allTop5Alerts.reduce((acc, current) => {
+   const alertTypes = Object.keys(current)
+
+   alertTypes.forEach(type => {
+     if(!acc[type]){
+      // @ts-ignore
+       acc[type] = current[type]
+     } else {
+      // @ts-ignore
+       if(acc[type] < current[type]){
+        // @ts-ignore
+         acc[type] = current[type]
+       }
+     }
+   })
+   
+   return acc
+  }, {})
+
+  return Object.fromEntries(Object.entries(aggTop5Alerts).sort((a: [string, number], b: [string, number]) => b[1] - a[1]).slice(0,5))
 }
