9d0f1d0b25e2046acc57418cb4ede7a1f60c1b6d
jdalton <john.david.dalton@gmail.com>
2024-10-15 16:53:58 -0400

    No longer need to set the NPM_VERSION env var

--- src/commands/npm.ts
+++ src/commands/npm.ts
@@ -1,4 +1,3 @@
-import { execSync } from 'node:child_process'
 import path from 'node:path'
 
 import spawn from '@npmcli/promise-spawn'
@@ -11,16 +10,9 @@ const description = 'npm wrapper functionality'
 export const npm: CliSubcommand = {
   description,
   async run(argv, _importMeta, _ctx) {
-    const npmVersion = execSync('npm -v').toString()
-    const wrapperPath = path.join(distPath, 'npm-cli.js')
-
     process.exitCode = 1
-
+    const wrapperPath = path.join(distPath, 'npm-cli.js')
     const spawnPromise = spawn(process.execPath, [wrapperPath, ...argv], {
-      env: {
-        ...process.env,
-        NPM_VERSION: npmVersion
-      },
       stdio: 'inherit'
     })
     spawnPromise.process.on('exit', (code, signal) => {
