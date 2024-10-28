f222bfa429876367c9df310efabfad34f0c0e2d7
jdalton <john.david.dalton@gmail.com>
2024-10-28 14:18:21 -0400

    Use tinyglobby instead of glob and fix glob pattern matching and tests

--- src/utils/path-resolve.ts
+++ src/utils/path-resolve.ts
@@ -1,132 +1,171 @@
-import { stat } from 'node:fs/promises'
+import fs from 'node:fs/promises'
 import path from 'node:path'
 
-import { globby } from 'globby'
 import ignore from 'ignore'
+import micromatch from 'micromatch'
+import { glob as tinyGlob } from 'tinyglobby'
 
-import { arrayUnique } from './arrays'
 import { directoryPatterns } from './ignore-by-default'
 
 import type { SocketYml } from '@socketsecurity/config'
 import type { SocketSdkReturnType } from '@socketsecurity/sdk'
-import type { Options as GlobbyOptions } from 'globby'
+import type { GlobOptions } from 'tinyglobby'
 
-const BASE_GLOBBY_OPTS = <GlobbyOptions>{
-  __proto__: null,
-  absolute: true,
-  expandDirectories: false,
-  gitignore: true,
-  ignore: directoryPatterns(),
-  markDirectories: true,
-  onlyFiles: true,
-  unique: true
+type GlobWithGitIgnoreOptions = GlobOptions & {
+  socketConfig?: SocketYml | undefined
 }
 
-export async function getPackageFiles(
-  cwd: string,
-  inputPaths: string[],
-  config: SocketYml | undefined,
-  supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data'],
-  debugLog: typeof console.error
-): Promise<string[]> {
-  debugLog(`Globbed resolving ${inputPaths.length} paths:`, inputPaths)
-
-  // TODO: Does not support `~/` paths
-  const entries = await globby(inputPaths, {
-    ...BASE_GLOBBY_OPTS,
-    cwd,
-    onlyFiles: false
-  })
-
-  debugLog(
-    `Globbed resolved ${inputPaths.length} paths to ${entries.length} paths:`,
-    entries
-  )
-
-  const packageFiles = await mapGlobResultToFiles(entries, supportedFiles)
-
-  debugLog(
-    `Mapped ${entries.length} entries to ${packageFiles.length} files:`,
-    packageFiles
-  )
-
-  const includedPackageFiles = config?.projectIgnorePaths?.length
-    ? ignore()
-        .add(config.projectIgnorePaths)
-        .filter(packageFiles.map(item => path.relative(cwd, item)))
-        .map((item: string) => path.resolve(cwd, item))
-    : packageFiles
-
-  return includedPackageFiles
-}
-
-export async function getPackageFilesFullScans(
-  cwd: string,
-  inputPaths: string[],
-  supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data'],
-  debugLog: typeof console.error
-): Promise<string[]> {
-  debugLog(`Globbed resolving ${inputPaths.length} paths:`, inputPaths)
-
-  // TODO: Does not support `~/` paths
-  const entries = await globby(inputPaths, {
-    ...BASE_GLOBBY_OPTS,
-    cwd,
-    onlyFiles: false
-  })
-
-  debugLog(
-    `Globbed resolved ${inputPaths.length} paths to ${entries.length} paths:`,
-    entries
-  )
-
-  const packageFiles = await mapGlobResultToFiles(entries, supportedFiles)
-
-  debugLog(
-    `Mapped ${entries.length} entries to ${packageFiles.length} files:`,
-    packageFiles
-  )
-
-  return packageFiles
-}
-
-export async function mapGlobResultToFiles(
+async function filterGlobResultToSupportedFiles(
   entries: string[],
   supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data']
 ): Promise<string[]> {
-  const packageFiles = await Promise.all(
-    entries.map(entry => mapGlobEntryToFiles(entry, supportedFiles))
+  const patterns = ['golang', 'npm', 'pypi'].reduce(
+    (r: string[], n: string) => {
+      const supported = supportedFiles[n]
+      r.push(
+        ...(supported
+          ? Object.values(supported).map(p => `**/${p.pattern}`)
+          : [])
+      )
+      return r
+    },
+    []
   )
-  return arrayUnique(packageFiles.flat())
+  return entries.filter(p => micromatch.some(p, patterns))
 }
 
-export async function mapGlobEntryToFiles(
-  entry: string,
-  supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data']
-): Promise<string[]> {
-  const jsSupported = supportedFiles['npm'] ?? {}
-  const jsLockFilePatterns = Object.values(jsSupported).map(
-    p => `**/${(p as { pattern: string }).pattern}`
-  )
-  const pyFilePatterns = Object.values(supportedFiles['pypi'] ?? {}).map(
-    p => `**/${(p as { pattern: string }).pattern}`
-  )
-  const goSupported = supportedFiles['golang'] ?? {}
-  const goSupplementalPatterns = Object.values(goSupported).map(
-    p => `**/${(p as { pattern: string }).pattern}`
-  )
-  return <string[]>(
-    await globby(
-      [...jsLockFilePatterns, ...pyFilePatterns, ...goSupplementalPatterns],
-      {
-        ...BASE_GLOBBY_OPTS,
-        onlyFiles: true,
-        cwd: path.resolve(
-          (await stat(entry)).isDirectory() ? entry : path.dirname(entry)
+async function globWithGitIgnore(
+  patterns: string[],
+  options: GlobWithGitIgnoreOptions
+) {
+  const {
+    socketConfig,
+    cwd = process.cwd(),
+    ...additionalOptions
+  } = <GlobWithGitIgnoreOptions>{
+    __proto__: null,
+    ...options
+  }
+  const projectIgnorePaths = socketConfig?.projectIgnorePaths
+  const ignoreFiles = await tinyGlob(['**/.gitignore'], <GlobOptions>{
+    __proto__: null,
+    absolute: true,
+    cwd,
+    expandDirectories: true
+  })
+  const ignores = [
+    ...directoryPatterns(),
+    ...(Array.isArray(projectIgnorePaths)
+      ? ignoreFileLinesToGlobPatterns(
+          projectIgnorePaths,
+          path.join(cwd, '.gitignore'),
+          cwd
         )
-      }
+      : []),
+    ...(
+      await Promise.all(
+        ignoreFiles.map(async filepath =>
+          ignoreFileToGlobPatterns(
+            await fs.readFile(filepath, 'utf8'),
+            filepath,
+            cwd
+          )
+        )
+      )
+    ).flat()
+  ]
+  const hasNegatedPattern = ignores.some(p => p.charCodeAt(0) === 33 /*'!'*/)
+  const globOptions = {
+    __proto__: null,
+    absolute: true,
+    cwd,
+    expandDirectories: false,
+    ignore: hasNegatedPattern ? [] : ignores,
+    ...additionalOptions
+  }
+  const result = await tinyGlob(patterns, globOptions)
+  if (!hasNegatedPattern) {
+    return result
+  }
+  const { absolute } = globOptions
+  const filtered = ignore()
+    .add(ignores)
+    .filter(absolute ? result.map(p => path.relative(cwd, p)) : result)
+  return absolute ? filtered.map(p => path.resolve(cwd, p)) : filtered
+}
+
+function ignoreFileLinesToGlobPatterns(
+  lines: string[],
+  filepath: string,
+  cwd: string
+): string[] {
+  const base = path.relative(cwd, path.dirname(filepath)).replace(/\\/g, '/')
+  const patterns = []
+  for (let i = 0, { length } = lines; i < length; i += 1) {
+    const pattern = lines[i]!.trim()
+    if (pattern.length > 0 && pattern.charCodeAt(0) !== 35 /*'#'*/) {
+      patterns.push(
+        ignorePatternToMinimatch(
+          pattern.length && pattern.charCodeAt(0) === 33 /*'!'*/
+            ? `!${path.posix.join(base, pattern.slice(1))}`
+            : path.posix.join(base, pattern)
+        )
+      )
+    }
+  }
+  return patterns
+}
+
+function ignoreFileToGlobPatterns(
+  content: string,
+  filepath: string,
+  cwd: string
+): string[] {
+  return ignoreFileLinesToGlobPatterns(content.split(/\r?\n/), filepath, cwd)
+}
+
+// Based on `@eslint/compat` convertIgnorePatternToMinimatch.
+// Apache v2.0 licensed
+// Copyright Nicholas C. Zakas
+// https://github.com/eslint/rewrite/blob/compat-v1.2.1/packages/compat/src/ignore-file.js#L28
+function ignorePatternToMinimatch(pattern: string): string {
+  const isNegated = pattern.startsWith('!')
+  const negatedPrefix = isNegated ? '!' : ''
+  const patternToTest = (isNegated ? pattern.slice(1) : pattern).trimEnd()
+  // Special cases.
+  if (
+    patternToTest === '' ||
+    patternToTest === '**' ||
+    patternToTest === '/**' ||
+    patternToTest === '**'
+  ) {
+    return `${negatedPrefix}${patternToTest}`
+  }
+  const firstIndexOfSlash = patternToTest.indexOf('/')
+  const matchEverywherePrefix =
+    firstIndexOfSlash === -1 || firstIndexOfSlash === patternToTest.length - 1
+      ? '**/'
+      : ''
+  const patternWithoutLeadingSlash =
+    firstIndexOfSlash === 0 ? patternToTest.slice(1) : patternToTest
+  // Escape `{` and `(` because in gitignore patterns they are just
+  // literal characters without any specific syntactic meaning,
+  // while in minimatch patterns they can form brace expansion or extglob syntax.
+  //
+  // For example, gitignore pattern `src/{a,b}.js` ignores file `src/{a,b}.js`.
+  // But, the same minimatch pattern `src/{a,b}.js` ignores files `src/a.js` and `src/b.js`.
+  // Minimatch pattern `src/\{a,b}.js` is equivalent to gitignore pattern `src/{a,b}.js`.
+  const escapedPatternWithoutLeadingSlash =
+    patternWithoutLeadingSlash.replaceAll(
+      /(?=((?:\\.|[^{(])*))\1([{(])/guy,
+      '$1\\$2'
     )
-  )
+  const matchInsideSuffix = patternToTest.endsWith('/**') ? '/*' : ''
+  return `${negatedPrefix}${matchEverywherePrefix}${escapedPatternWithoutLeadingSlash}${matchInsideSuffix}`
+}
+
+function pathsToPatterns(paths: string[]): string[] {
+  return paths.map(p => (p === '.' ? '**/*' : p))
 }
 
 export function findRoot(filepath: string): string | undefined {
@@ -141,4 +180,68 @@ export function findRoot(filepath: string): string | undefined {
     }
     curPath = parent
   }
+}
+
+export async function getPackageFiles(
+  cwd: string,
+  inputPaths: string[],
+  config: SocketYml | undefined,
+  supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data'],
+  debugLog: typeof console.error = () => {}
+): Promise<string[]> {
+  debugLog(`Globbed resolving ${inputPaths.length} paths:`, inputPaths)
+
+  // TODO: Does not support `~/` paths
+  const entries = await globWithGitIgnore(pathsToPatterns(inputPaths), {
+    cwd,
+    socketConfig: config
+  })
+
+  debugLog(
+    `Globbed resolved ${inputPaths.length} paths to ${entries.length} paths:`,
+    entries
+  )
+
+  const packageFiles = await filterGlobResultToSupportedFiles(
+    entries,
+    supportedFiles
+  )
+
+  debugLog(
+    `Mapped ${entries.length} entries to ${packageFiles.length} files:`,
+    packageFiles
+  )
+
+  return packageFiles
+}
+
+export async function getPackageFilesFullScans(
+  cwd: string,
+  inputPaths: string[],
+  supportedFiles: SocketSdkReturnType<'getReportSupportedFiles'>['data'],
+  debugLog: typeof console.error = () => {}
+): Promise<string[]> {
+  debugLog(`Globbed resolving ${inputPaths.length} paths:`, inputPaths)
+
+  // TODO: Does not support `~/` paths
+  const entries = await globWithGitIgnore(pathsToPatterns(inputPaths), {
+    cwd
+  })
+
+  debugLog(
+    `Globbed resolved ${inputPaths.length} paths to ${entries.length} paths:`,
+    entries
+  )
+
+  const packageFiles = await filterGlobResultToSupportedFiles(
+    entries,
+    supportedFiles
+  )
+
+  debugLog(
+    `Mapped ${entries.length} entries to ${packageFiles.length} files:`,
+    packageFiles
+  )
+
+  return packageFiles
 }
