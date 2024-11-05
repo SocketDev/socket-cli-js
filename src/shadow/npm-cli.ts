d13ad4d6d1a4825a66b29824c76587c867ee439e
jdalton <john.david.dalton@gmail.com>
2024-11-05 14:20:18 -0500

    Update npm fixes based on https://github.com/npm/cli/pull/7025

--- src/shadow/npm-cli.ts
+++ src/shadow/npm-cli.ts
@@ -17,27 +17,31 @@ const injectionPath = path.join(realDirname, 'npm-injection.js')
 // Adding the `--quiet` and `--no-progress` flags when the `proc-log` module
 // is found to fix a UX issue when running the command with recent versions of
 // npm (input swallowed by the standard npm spinner)
-let npmArgs: string[] = []
-if (process.argv.slice(2).includes('install')) {
+const npmArgs: string[] = process.argv.slice(2)
+if (
+  npmArgs.includes('install') &&
+  !npmArgs.includes('--no-progress') &&
+  !npmArgs.includes('--quiet')
+) {
   const npmEntrypoint = realpathSync(npmPath)
   const npmRootPath = findRoot(path.dirname(npmEntrypoint))
   if (npmRootPath === undefined) {
     process.exit(127)
   }
   const npmDepPath = path.join(npmRootPath, 'node_modules')
-  let npmlog
+  let procLog
   try {
-    npmlog = require(path.join(npmDepPath, 'proc-log/lib/index.js')).log
+    procLog = require(path.join(npmDepPath, 'proc-log/lib/index.js')).log
   } catch {}
-  if (npmlog) {
-    npmArgs = ['--quiet', '--no-progress']
+  if (procLog) {
+    npmArgs.push('--no-progress', '--quiet')
   }
 }
 
 process.exitCode = 1
 const spawnPromise = spawn(
   process.execPath,
-  ['--require', injectionPath, npmPath, ...process.argv.slice(2), ...npmArgs],
+  ['--require', injectionPath, npmPath, ...npmArgs],
   {
     stdio: 'inherit'
   }
