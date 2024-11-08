8210695291645528eb5475add0634307d4834c9e
jdalton <john.david.dalton@gmail.com>
2024-11-07 21:49:37 -0500

    Use tiny-updater

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-import { readFileSync } from 'node:fs'
 import path from 'node:path'
 import { pathToFileURL } from 'node:url'
 
 import chalk from 'chalk'
 import { messageWithCauses, stackWithCauses } from 'pony-cause'
-import updateNotifier from 'update-notifier'
+import updateNotifier from 'tiny-updater'
 
 import * as cliCommands from './commands'
 import { logSymbols } from './utils/chalk-markdown'
@@ -15,6 +14,9 @@ import { meowWithSubcommands } from './utils/meow-with-subcommands'
 
 const distPath = __dirname
 const rootPath = path.resolve(distPath, '..')
+const rootPkgJsonPath = path.join(rootPath, 'package.json')
+
+const rootPkgJson = require(rootPkgJsonPath)
 
 const formattedCliCommands = Object.fromEntries(
   Object.entries(cliCommands).map(entry => {
@@ -29,11 +31,11 @@ function camelToHyphen(str: string): string {
 
 // TODO: Add autocompletion using https://socket.dev/npm/package/omelette
 void (async () => {
-  try {
-    updateNotifier({
-      pkg: JSON.parse(readFileSync(path.join(rootPath, 'package.json'), 'utf8'))
-    }).notify()
-  } catch {}
+  await updateNotifier({
+    name: rootPkgJson.name,
+    version: rootPkgJson.version,
+    ttl: 86_400_000 /* 24 hours in milliseconds */
+  })
 
   try {
     await meowWithSubcommands(formattedCliCommands, {
@@ -48,10 +50,9 @@ void (async () => {
       importMeta: { url: `${pathToFileURL(__filename)}` } as ImportMeta
     })
   } catch (err) {
+    let errorBody: string | undefined
     let errorTitle: string
     let errorMessage = ''
-    let errorBody: string | undefined
-
     if (err instanceof AuthError) {
       errorTitle = 'Authentication error'
       errorMessage = err.message
@@ -66,14 +67,12 @@ void (async () => {
     } else {
       errorTitle = 'Unexpected error with no details'
     }
-
     console.error(
       `${logSymbols.error} ${chalk.white.bgRed(errorTitle + ':')} ${errorMessage}`
     )
     if (errorBody) {
       console.error(`\n${errorBody}`)
     }
-
     process.exit(1)
   }
 })()
