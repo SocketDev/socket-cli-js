4127ada244e83762fba43a117f80a1fd437737e3
jdalton <john.david.dalton@gmail.com>
2024-12-05 15:31:06 -0500

    Support sync-module

--- .config/rollup.test.config.mjs
+++ .config/rollup.test.config.mjs
@@ -1,7 +1,13 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+import { isRelative } from '@socketsecurity/registry/lib/path'
+
 import baseConfig from './rollup.base.config.mjs'
+import constants from '../scripts/constants.js'
+import { normalizeId, isBuiltin } from '../scripts/utils/packages.js'
+
+const { ROLLUP_EXTERNAL_SUFFIX, SUPPORTS_SYNC_ESM } = constants
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
@@ -23,5 +29,16 @@ export default () =>
         externalLiveBindings: false,
         freeze: false
       }
-    ]
+    ],
+    ...(SUPPORTS_SYNC_ESM
+      ? {
+          external(id_) {
+            if (id_.endsWith(ROLLUP_EXTERNAL_SUFFIX) || isBuiltin(id_)) {
+              return true
+            }
+            const id = normalizeId(id_)
+            return !(isRelative(id) || id.startsWith(srcPath))
+          }
+        }
+      : {})
   })
