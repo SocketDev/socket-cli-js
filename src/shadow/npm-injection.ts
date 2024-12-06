1c84899e5848c5c516fbebceba4e1f355b2526c0
jdalton <john.david.dalton@gmail.com>
2024-12-06 15:48:57 -0500

    Minor comment nit.

--- src/shadow/npm-injection.ts
+++ src/shadow/npm-injection.ts
@@ -2,6 +2,6 @@ import { shadowBinPath } from '../constants'
 import { installSafeArborist } from './arborist'
 import { installLinks } from './link'
 
-// shadow `npm` and `npx` to mitigate subshells
+// Shadow `npm` and `npx` to mitigate subshells.
 installLinks(shadowBinPath, 'npm')
 installSafeArborist()
