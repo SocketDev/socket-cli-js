edd636db0bd39f07529258c917695fa54c938d54
jdalton <john.david.dalton@gmail.com>
2024-12-07 17:44:40 -0500

    Add params to npm commands to make it less chatty

--- test/socket-npm.test.cjs
+++ test/socket-npm.test.cjs
@@ -5,42 +5,49 @@ const { spawnSync } = require('node:child_process')
 const path = require('node:path')
 const { describe, it } = require('node:test')
 
+const spawn = require('@npmcli/promise-spawn')
+
 const constants = require('../scripts/constants')
 const { distPath } = constants
 
 const testPath = __dirname
 const entryPath = path.join(distPath, 'cli.js')
-
-function spawnNPM({ args = [], cwd, installDir }) {
-  return spawnSync(process.execPath, [entryPath, 'npm', ...args], {
-    cwd: path.join(testPath, cwd),
-    encoding: 'utf8',
-    env: {
-      // Make sure we don't borrow TTY from parent.
-      SOCKET_SECURITY_TTY_IPC: undefined,
-      PATH: `${path.join(installDir, 'node_modules', '.bin')}:${process.env.PATH}`
-    },
-    stdio: ['pipe', 'pipe', 'pipe']
-  })
-}
+const npmFixturesPath = path.join(testPath, 'socket-npm-fixtures')
 
 // These aliases are defined in package.json.
 for (const npm of ['npm8', 'npm10']) {
-  const installDir = path.join(testPath, `/socket-npm-fixtures/${npm}`)
-  spawnSync('npm', ['install'], {
-    cwd: installDir,
-    stdio: 'ignore'
-  })
+  const npmPath = path.join(npmFixturesPath, npm)
+  const npmBinPath = path.join(npmPath, 'node_modules', '.bin')
+
+  spawnSync(
+    'npm',
+    ['install', '--no-audit', '--no-fund', '--no-progress', '--quiet'],
+    {
+      cwd: npmPath,
+      stdio: 'ignore'
+    }
+  )
 
   describe(`Socket npm wrapper for ${npm}`, () => {
-    it('should bail on new typosquat', () => {
-      const ret = spawnNPM({
-        cwd: './socket-npm-fixtures/lacking-typosquat',
-        installDir,
-        args: ['i', 'bowserify']
-      })
-      assert.strictEqual(ret.status, 1, ret.stderr)
-      assert.ok(ret.stderr.includes('Unable to prompt'), ret.stderr)
+    it('should bail on new typosquat', async () => {
+      try {
+        await spawn(
+          process.execPath,
+          [entryPath, 'npm', 'install', 'bowserify'],
+          {
+            cwd: path.join(npmFixturesPath, 'lacking-typosquat'),
+            encoding: 'utf8',
+            env: {
+              // Make sure we don't borrow TTY from parent.
+              SOCKET_SECURITY_TTY_IPC: undefined,
+              PATH: `${npmBinPath}:${process.env.PATH}`
+            }
+          }
+        )
+        assert.ok(false, 'typosquat not error')
+      } catch (e) {
+        assert.ok(e?.stderr.includes('Unable to prompt'), e?.stderr)
+      }
     })
   })
 }
