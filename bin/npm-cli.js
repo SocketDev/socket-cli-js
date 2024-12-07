de5f846e62b100df2c52fdc9e30480e302d35c8b
jdalton <john.david.dalton@gmail.com>
2024-12-06 20:01:57 -0500

    Remove process.removeAllListeners('warning') from bin stubs

--- bin/npm-cli.js
+++ bin/npm-cli.js
@@ -5,5 +5,4 @@ const semver = require('semver')
 const distType = semver.satisfies(process.versions.node, '>=22.12')
   ? 'module-sync'
   : 'require'
-process.removeAllListeners('warning')
 require(`../dist/${distType}/npm-cli.js`)
